ab53fdf569213e65e8336fdfcd188fd96d9f4304
OsamahBagah-BackEnd <osamahbagah@gmail.com>
2026-01-14 23:30:31 +0300

    first commit from my device

---
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server=DESKTOP-8DIGU3M;Database=DVLD;User Id=Sa;Password=123;";
+
+
+    }
+}
---
+++ DVLD_DataAccess/clsEventLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+
+public static class clsEventLog
+{
+    public static void EventLogException(string Message, EventLogEntryType EventLogEntryType)
+    {
+        string SourceName = "DVLD App";
+        try
+        {
+            if (!EventLog.SourceExists(SourceName))
+            {
+                EventLog.CreateEventSource(SourceName, "Application");
+
+            }
+            EventLog.WriteEntry(SourceName, Message, EventLogEntryType);
+        }
+        catch (Exception ex)
+        {
+            EventLog.WriteEntry(SourceName, "Exception in logException Method : " + ex.Message, EventLogEntryType);
+        }
+    }
+}
+
